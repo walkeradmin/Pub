9a8b91e580c5130664073542e770d3d22d0bdd32
walker <im.adam.watson@gmail.com>
2019-09-18 10:02:39 +0800

    UPDATE

--- bash.sh
+++ bash.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-for (i=0;i<=10;i++);
-do
-sum=((sum+i))
-done
-echo $sum
+read -p "Please input your grade" gd
+if [ $gd -ge 90 ] && [ $gd -le 100 ];
+then
+    echo "Great"
+elif [ $gd -ge 70 ] && [ $gd -lt 90 ];
+then
+    echo "Simple"
+else
+    echo "Failed"
+fi
